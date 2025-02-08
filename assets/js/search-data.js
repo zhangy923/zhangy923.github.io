5b3668d03052c098ced72a6b6240e00f0f682caa
zhangy923 <33924592+zhangy923@users.noreply.github.com>
2025-02-08 02:01:56 +0000

    Deploying to gh-pages from @ zhangy923/zhangy923.github.io@6f5ae600fb2e09de1b8e72e69294aabef0940c02 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -368,16 +368,8 @@ ninja.data = [{
           window.location.href = "/blog/2015/formatting-and-links/";
         
       },
-    },{id: "news-a-simple-inline-announcement",
-          title: 'A simple inline announcement.',
-          description: "",
-          section: "News",},{id: "news-a-long-announcement-with-details",
-          title: 'A long announcement with details',
-          description: "",
-          section: "News",handler: () => {
-              window.location.href = "/news/announcement_2/";
-            },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
-          title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
+    },{id: "news-one-paper-was-accepted-by-the-aaai-2025-conference",
+          title: 'One paper was accepted by the AAAI 2025 conference!',
           description: "",
           section: "News",},{id: "projects-project-1",
           title: 'project 1',
